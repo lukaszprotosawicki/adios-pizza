7f282357f4e1931a213754d5d7c6996c0a848638
lukasz protosawicki <lukas.es.p@o2.pl>
2021-02-23 20:27:53 +0100

    add styles and text and fonts to containerbody

--- src/components/ContainerBody.js
+++ src/components/ContainerBody.js
@@ -5,12 +5,18 @@ const ContainerBody = () => {
   return (
     <>
       <div className="backgrond-body">
-        <img src={backgrondBody} alt="Adios Pizza" />
+        <img
+          className="background-body-img"
+          src={backgrondBody}
+          alt="Adios Pizza"
+        />
         <div className="container-body">
-          <span>Wyjątkowa kuchnia, w której mile zaskoczymy</span>
+          <span className="text-kitchen">
+            Wyjątkowa kuchnia, w której mile zaskoczymy
+          </span>
           <p></p>
-          <span>NIE TYLKO SMAKOSZY PIZZY</span>
-          <p></p>
+          <span className="text-pizza">NIE TYLKO SMAKOSZY PIZZY</span>
+          <p className="border-bottom"></p>
           <button>ZOBACZ MENU</button>
         </div>
       </div>
