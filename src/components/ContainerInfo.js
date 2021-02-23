2da046b53cc8c8c0a1f0bddadbbe0a16407c0a37
lukasz protosawicki <lukas.es.p@o2.pl>
2021-02-23 21:10:07 +0100

    add styles and text to body info

--- src/components/ContainerInfo.js
+++ src/components/ContainerInfo.js
@@ -1,9 +1,43 @@
 import React from "react";
+import backgrondInfo1 from "../img/pic1.jpg";
+import backgrondInfo2 from "../img/pic2.jpg";
 
 const ContainerInfo = () => {
   return (
     <>
-      <div className="backgrond-body-info"></div>
+      <div className="backgrond-body-info">
+        <div className="content-position">
+          <div className="content-info">
+            <div className="styles-welcome">
+              <p>
+                Zapraszamy do zamówień telefonicznych <p>lub</p>{" "}
+                <p>odbiób osobisty w naszej pizzerii!!</p>
+              </p>
+            </div>
+            <div className="styles-contact">
+              <p>🍔🍕🍟 Adios Pizza </p>
+              <p>
+                🍔🍕🍟 Gorzów Wielkopolski ul. Pułaskiego 50 (Lokal od ulicy
+                Wróblewskiego)
+              </p>
+              <p>🍔🍕 www.adios-pizza.pl </p>
+              <p>🍔🍕 tel: 796 71 76 50</p>
+            </div>
+          </div>
+          <div className="content-gallery">
+            <img
+              className="background-info-img1"
+              src={backgrondInfo1}
+              alt="Adios Pizza"
+            />
+            <img
+              className="background-info-img2"
+              src={backgrondInfo2}
+              alt="Adios Pizza"
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
