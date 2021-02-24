2a8f4675d7307e1bf553eb122f782a188974ebc7
lukasz protosawicki <lukas.es.p@o2.pl>
2021-02-24 12:12:26 +0100

    add router, switch, navlink

--- src/App.js
+++ src/App.js
@@ -5,17 +5,24 @@ import ContainerInfo from "./components/ContainerInfo";
 import ContainerGallery from "./components/ContainerGallery";
 import Footer from "./components/Footer.js";
 import FooterInfo from "./components/FooterInfo.js";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <MainPages />
       <ContainerBody />
       <ContainerInfo />
       <ContainerGallery />
       <Footer />
       <FooterInfo />
-    </>
+      <Switch>
+        {/* <Route exact path="/" component={} />
+
+        <Route path="/login" component={} />
+        <Route path="/signup" component={} /> */}
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/MainPages.js
+++ src/components/MainPages.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as NavLink } from "react-router-dom";
 import logo from "../img/logo6.jpg";
 
 const MainPages = () => {
@@ -8,6 +9,18 @@ const MainPages = () => {
         <div className="ui_container">
           <div className="logo">
             {" "}
+            <nav>
+              <NavLink to={"/"}> About Me</NavLink>
+              <NavLink activeClassName="active-link" to={"/projects"}>
+                Projects
+              </NavLink>
+              <NavLink activeClassName="active-link" to={"/contact"}>
+                Contact
+              </NavLink>
+              <NavLink activeClassName="active-link" to={"/weather"}>
+                Weather
+              </NavLink>
+            </nav>
             <a href="/">
               <img src={logo} alt="Adios Pizza" />
             </a>
