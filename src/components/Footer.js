1bda2cc7cc7f9fdfd08ad689b60f47f9065c7996
lukasz protosawicki <lukas.es.p@o2.pl>
2021-02-26 14:16:53 +0100

    mixed changes styles footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,29 +6,32 @@ const Footer = () => {
     <>
       <div className="backgrond-body-footer">
         <div className="container-footer">
-          <div className="">
+          <div className="logo-footer">
             {" "}
             <a href="/">
               <img src={logo} alt="Adios Pizza" />
             </a>
           </div>
-
-          <div className="styles-welcome">
-            <p>
-              Zapraszamy do zamówień telefonicznych <p>lub</p>{" "}
-              <p>odbiór osobisty w naszej pizzerii!!</p>
-            </p>
+          <div className="welcome-contact">
+            <div className="styles-welcome">
+              <p>
+                Zapraszamy do zamówień telefonicznych <p>lub</p>{" "}
+                <p>odbiór osobisty w naszej pizzerii!!</p>
+              </p>
+            </div>
+            <div id="contact" className="styles-contact">
+              <p>🍔🍕🍟 Adios Pizza - Pizzeria</p>
+              <p>
+                🍔🍕🍟 Gorzów Wielkopolski ul. Pułaskiego 50 (Lokal od ulicy
+                Wróblewskiego)
+              </p>
+              <p>🍔🍕 www.adios-pizza.pl </p>
+              <p>🍔🍕 tel: 796 71 76 50</p>
+            </div>
           </div>
-          <div id="contact" className="styles-contact">
-            <p>🍔🍕🍟 Adios Pizza - Pizzeria</p>
-            <p>
-              🍔🍕🍟 Gorzów Wielkopolski ul. Pułaskiego 50 (Lokal od ulicy
-              Wróblewskiego)
-            </p>
-            <p>🍔🍕 www.adios-pizza.pl </p>
-            <p>🍔🍕 tel: 796 71 76 50</p>
+          <div>
+            <button className="buttof-footer">PRZEJDŹ DO MENU</button>
           </div>
-          <button>PRZEJDŹ DO MENU</button>
         </div>
       </div>
     </>
